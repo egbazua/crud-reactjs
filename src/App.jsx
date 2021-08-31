ec2991bf1e834c9731cd118a7987a73018e917be
Enrique Gómez Bazúa <enriquegbazua@gmail.com>
2021-08-31 01:09:42 -0700

    Button details

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ function App() {
 
   const [task, setTask] = useState('');
   const [taskForm, setTaskForm] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [id, setId] = useState('');
 
   const addTask = (e) => {
-    e.preventDefault;
+    e.preventDefault();
 
     if(!task.trim()){
       return console.log("empty element")
@@ -15,12 +17,42 @@ function App() {
 
     console.log(task);
 
-    setTastkForm([
+    setTaskForm([
       ...taskForm,
-      {id: nanoid.generate(10), taskName: task}
+      {id: nanoid(), taskName: task}
     ])
 
     setTask('');
+  }
+
+  const deleteTask = (id) => {
+    console.log(id);
+
+    const filterArray = taskForm.filter(item => item.id !== id);
+    setTaskForm(filterArray);
+  }
+
+  const taskEditMode = (item) => {
+    console.log(item);
+
+    setEditMode(true);
+    setTask(item.taskName);
+    setId(item.id)
+  }
+
+  const editTask = (e) => {
+    e.preventDefault();
+
+    if(!task.trim()){
+      return console.log("empty element")
+    }
+
+    const editedArray = taskForm.map((item) => (item.id === id ? { id, taskName: task} : item))
+
+     setTaskForm(editedArray);
+     setEditMode(false);
+     setTask('');
+     setId('');
   }
 
   return (
@@ -30,20 +62,32 @@ function App() {
         <hr />
         <div className="row">
           <div className="col-8">
-            <h4 className="text-center"> Taks List </h4>
+            <h4 className="text-center"> Task List </h4>
             <ul className="list-group">
-              <li className="list-group-item">
-                <span className="lead"> Task Name </span>
-                <button className="btn btn-danger btn-sm float-end mx-2">Delete</button>
-                <button className="btn btn-warning btn-sm float-end">Edit</button>
-              </li>
+              {
+                taskForm.map(item => (
+                  <li className="list-group-item mt-1" key={item.id}>
+                    <span className="lead"> {item.taskName} </span>
+                    <button className="btn btn-danger btn-sm float-end mx-2" onClick={() => deleteTask(item.id)} >Delete</button>
+                    <button className="btn btn-warning btn-sm float-end" onClick={() => taskEditMode(item)}>Edit</button>
+                </li>
+                ))
+              }
             </ul>
           </div>
           <div className="col-4">
-            <h4 className="text-center"> Form </h4>
-            <form onSubmit={addTask}>
+            <h4 className="text-center">
+              {
+                editMode ? 'Edit Task' : 'Add Task'
+              }
+            </h4>
+            <form onSubmit={editMode ? editTask : addTask}>
               <input type="text" onChange={ e => setTask(e.target.value) } value={task} className="form-control mb-2" placeholder="Enter Task"/>
-              <button className="btn btn-dark w-100" type="submit">Add</button>
+              {
+                editMode ? (
+                  <button className="btn w-100 btn-warning" type="submit">Edit</button>    
+                ) : (<button className="btn btn-dark w-100" type="submit">Add</button>)
+              }
             </form>
           </div>
         </div>
